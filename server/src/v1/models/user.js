2a4398373e906eb51e308904adec09e7d789274f
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-08-23 16:53:08 +0700

    update User

--- server/src/v1/models/user.js
+++ server/src/v1/models/user.js
@@ -1,16 +1,22 @@
 const mongoose = require("mongoose");
 const { schemaOptions } = require("./modelOptions");
 
-const userSchema = new mongoose.Schema({
-    userName: {
-        type: String,
-        require: true,
-        unique: true,
-    },
+const userSchema = new mongoose.Schema(
+    {
+        userName: {
+            type: String,
+            required: true,
+            unique: true,
+        },
 
-    password: {
-        type: String,
-        require: true,
-        unique: true,
+        password: {
+            type: String,
+            required: true,
+            select: false,
+        },
     },
-});
+    schemaOptions
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
